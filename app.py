ff1091a093edc71a8a1b9d579eb0190d44ea3dec
Cailean Finn <Caileannn@gmail.com>
2024-05-09 15:30:32 +0100

    fixed structure and changed styling (color, font)

--- app.py
+++ app.py
@@ -12,11 +12,48 @@ class WikiApp(Flask):
         super().__init__(*args, **kwargs)
                 
         # Define routes
-        self.route('/', methods=['GET'])(self.homepage)
+        # self.route('/', methods=['GET'])(self.homepage)
+        self.route('/', methods=['GET'])(self.homepage_new)
+        self.route('/publications', methods=['GET'])(self.fetch_publications)
+        self.route('/meetups', methods=['GET'])(self.fetch_meetups)
         self.route('/<string:title>', methods=['GET'])(self.page_content)
         self.route('/favicon.ico')(self.favicon)
         self.route('/archive/<string:collection>', methods=['GET'])(self.get_collection)
     
+    def homepage_new(self):
+        pages = ['Homepage']
+        homepage_content = ''
+        for page in pages:
+            # Make a request to MediaWiki API to get content of a specific page
+            response = requests.get(self.MEDIAWIKI_BASE_URL + self.BASE_API, params={'action': 'parse', 'page': page, 'format': 'json'})
+            data = response.json()
+            # Extract page title and content
+            page_content = data['parse']['text']['*']
+            page_content = self.fix_html(page_content)
+            homepage_content += page_content
+            
+        return render_template('cn-home.html', nav_elements=self.get_nav_menu(), content=homepage_content)
+        
+    def fetch_publications(self):
+        concepts = ['Newsletters', 'Projects']
+        publication_page_list = self.fetch_all_pages(concepts)
+        updated_cat_list = self.fetch_pages_cat(publication_page_list)
+        projects = updated_cat_list.get('Projects', [])
+        newsletters = updated_cat_list.get('Newsletters', [])
+        nav_elements = self.get_nav_menu()
+        
+        return render_template('publications.html', projects=projects, newsletters=newsletters, nav_elements=nav_elements)
+    
+    def fetch_meetups(self):
+        concepts = ['Meetups']
+        # publication_page_list = self.fetch_all_pages(concepts)
+        # updated_cat_list = self.fetch_pages_cat(publication_page_list)
+        # meetups = updated_cat_list.get('Meetups', [])
+        nav_elements = self.get_nav_menu() 
+        meetup_content = self.fetch_page('Meetups')
+        
+        return render_template('meetups.html', content=meetup_content, nav_elements=nav_elements)
+        
     def fetch_pages_cat(self, category_page_list):
         all_pages_string = '|'.join(page for pages in category_page_list.values() for page in pages)
         thumb_resp = requests.get(self.MEDIAWIKI_BASE_URL + self.BASE_API, params={
@@ -41,15 +78,12 @@ class WikiApp(Flask):
                             category_page_list[category][page_title].update({'pageid':pageid, 'title': title, 'source': source })
                             
         return category_page_list
-
-            
-            
+      
     def fetch_all_pages(self, categories):
         category_page_list = {} 
         for category in categories:
             response = requests.get(self.MEDIAWIKI_BASE_URL + self.BASE_API, params={'action': 'ask', 'query': '[[Concept:'+category+']]|?Article:Date', 'format': 'json', 'formatversion': '2'})
             data = response.json()
-            print(data)
             page_title_timestamps = {}
             for page_title, page_data in data['query']['results'].items():
                 if 'printouts' in page_data and 'Article:Date' in page_data['printouts']:
@@ -61,8 +95,7 @@ class WikiApp(Flask):
                     
             category_page_list[category] = page_title_timestamps
         return category_page_list
-            
-    
+             
     def homepage(self):
         # Fetch pages for articles, projects, and newsletters
         categories = ['Articles', 'Projects', 'Newsletters']
@@ -85,13 +118,25 @@ class WikiApp(Flask):
         page_content = self.fix_html(page_content)
         return render_template('article.html', nav_elements=self.get_nav_menu(), title=page_title, content=page_content)
     
+    def fetch_page(self, title):
+        # Make a request to MediaWiki API to get content of a specific page
+        response = requests.get(self.MEDIAWIKI_BASE_URL + self.BASE_API, params={'action': 'parse', 'page': title, 'format': 'json'})
+        data = response.json()
+        # Extract page title and content
+        page_title = data['parse']['title']
+        page_content = data['parse']['text']['*']
+        page_content = self.fix_html(page_content)
+        return page_content
+    
     def get_nav_menu(self):
         response = requests.get(self.MEDIAWIKI_BASE_URL + self.BASE_API, params={'action': 'ask', 'query': '[[Concept:MainNavigation]]', 'format': 'json', 'formatversion': '2'})
         data = response.json()
         main_navigation_elements = {}
         for page_title, page_data in data['query']['results'].items():
             main_navigation_elements[page_title] = {'title':page_data.get('fulltext', '')}
-        return main_navigation_elements
+        reversed_main_navigation = list(main_navigation_elements.items())[::-1]
+        reversed_main_navigation = dict(reversed_main_navigation)
+        return reversed_main_navigation
     
     def fix_html(self, page_content):
         soup = BeautifulSoup(page_content, 'html.parser')
@@ -110,7 +155,7 @@ class WikiApp(Flask):
         for link in links:
             # Add _blank to href
             link['target'] = '_blank'
-            link.string = link.string.strip() + " ↘"
+            link.string = link.string.strip() + " ↗"
             
         # Find all a tags with href containing 'index.php'
         links = soup.find_all('a', href=lambda href: href and 'index.php' in href)
